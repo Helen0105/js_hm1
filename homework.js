86b3c3d8553e4f1fa5be6dd6023bee9ea84c682d
Helen0105 <marinenkoolena21@gmail.com>
2025-08-31 13:48:31 +0300

    first commit

---
+++ homework.js
@@ -0,0 +1,17 @@
+// const name = "Olena";
+// console.log(name);
+// let lastName = "Marynenko";
+// console.log(lastName);
+// console.log(typeof lastName);
+// const number = 22;
+// console.log(number);
+// console.log(typeof number);
+// const number2 = 5 > 2;
+// console.log(number2);
+
+
+const age = "13"
+console.log(age);
+const name = "Olena";
+console.log(name);
+
