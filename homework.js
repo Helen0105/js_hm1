6829351f7d2c741e3ffe0d0c6ccc4b79b75422d4
Helen0105 <marinenkoolena21@gmail.com>
2025-09-06 16:30:31 +0300

    homework

--- homework.js
+++ homework.js
@@ -12,6 +12,32 @@
 
 const age = "13"
 console.log(age);
+//
 const name = "Olena";
 console.log(name);
-
+//
+const isStudent = "true"; 
+console.log(isStudent);
+//
+const myString = "Якщо в житті немає задоволення, то повинен бути хоч якийсь сенс.";
+console.log(myString);
+//
+let myNumber = 17; 
+myNumber = myNumber + 10; 
+console.log(myNumber); 
+//
+const myNull = "null";
+console.log(myNull);
+//
+const userName = prompt("Введіть своє ім’я");
+alert("Привіт, " + userName + "!");
+console.log(typeof userName);
+//
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+const userConfirmation = confirm("Ви впевнені, що хочете продовжити?");
+console.log("Користувач підтвердив дію:", userConfirmation);
+//
+alert("Увага! Дія може бути небезпечною.");
+const userConfirmation = confirm("Ви впевнені, що хочете продовжити?");
+console.log("Користувач підтвердив дію:", userConfirmation);
